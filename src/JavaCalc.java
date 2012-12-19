54f9da64a16e52b7e0a23738fe1d9c9d04cca4b1
Leon Byford <ldjb20@bath.ac.uk>
2012-12-19 19:32:16 +0000

    Somewhat functional single-digit RPN calculator

---
+++ src/JavaCalc.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaCalc {
+	public static void main(String[] args) throws IOException, NumberFormatException{
+// 		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+// 		String input = stdin.readLine();
+// 		Double output = new Double(input) + 3.2;
+// 		System.out.println(output);
+		
+		Character[] arrInput = {'1', '3', '/', '1', '/'};
+		Stack<Double> stkNums = new Stack<Double>();
+		for (Character i : arrInput) {
+			if (Character.isDigit(i)) {
+				stkNums.push(new Double(Character.getNumericValue(i)));
+			}
+			else if (i == '+') {
+				Double var1 = stkNums.pop();
+				Double var2 = stkNums.pop();
+				stkNums.push(var2+var1);
+			}
+			else if (i == '-') {
+				Double var1 = stkNums.pop();
+				Double var2 = stkNums.pop();
+				stkNums.push(var2-var1);
+			}
+			else if (i == '*') {
+				Double var1 = stkNums.pop();
+				Double var2 = stkNums.pop();
+				stkNums.push(var2*var1);
+			}
+			else if (i == '/') {
+				Double var1 = stkNums.pop();
+				Double var2 = stkNums.pop();
+				stkNums.push(var2/var1);
+			}
+		}
+		System.out.println(stkNums.pop());
+	}
+}
