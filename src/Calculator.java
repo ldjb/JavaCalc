382ec2b163a57acdc7468b3eaa3d5653d5365ca8
Leon Byford <ldjb20@bath.ac.uk>
2012-12-22 16:50:28 +0000

    allow input via CLI.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,24 @@
-//import java.io.*;
-//import java.util.*;
+import java.io.*;
+import java.util.*;
 
 public class Calculator {
-	public static void main(String[] args) {
-		//BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-		//String input = stdin.readLine();
-		//char[] arrInput = input.toCharArray();
-		Object[] arrInput = {-5.0, 3.0, "+"};
-		System.out.println(RPNcalc.eval(arrInput));
+	public static void main(String[] args) throws IOException {
+		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			String input = stdin.readLine();
+			//char[] arrInput = input.toCharArray();
+			//Object[] arrInput = {-5.0, 3.0, "+"};
+			//System.out.println(RPNcalc.eval(arrInput));
+			ArrayList<Object> arrInput = new ArrayList<Object>();
+			for (char i : input.toCharArray()) {
+				if (Character.isDigit(i)) {
+					arrInput.add(new Double(Character.getNumericValue(i)));
+				}
+				else {
+					arrInput.add(Character.toString(i));
+				}
+			}
+			System.out.println(RPNcalc.eval(arrInput.toArray()));
+		}
 	}
 }
