44a4f30a5625a6b6a6efc608006850a00770e88a
Leon Byford <ldjb20@bath.ac.uk>
2012-12-22 13:54:30 +0000

    class should be called "Calculator.java"

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,7 @@
 //import java.io.*;
 //import java.util.*;
 
-public class JavaCalc {
+public class Calculator {
 	public static void main(String[] args) {
 		//BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
 		//String input = stdin.readLine();
