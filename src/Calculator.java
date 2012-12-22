0943e26ccb5b6e29dc73cf807ff17ce426114b8b
Leon Byford <ldjb20@bath.ac.uk>
2012-12-22 16:54:26 +0000

    Calculator.java cleanup

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,12 +3,10 @@ import java.util.*;
 
 public class Calculator {
 	public static void main(String[] args) throws IOException {
-		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader stdin = new BufferedReader(
+							   new InputStreamReader(System.in));
 		while (true) {
 			String input = stdin.readLine();
-			//char[] arrInput = input.toCharArray();
-			//Object[] arrInput = {-5.0, 3.0, "+"};
-			//System.out.println(RPNcalc.eval(arrInput));
 			ArrayList<Object> arrInput = new ArrayList<Object>();
 			for (char i : input.toCharArray()) {
 				if (Character.isDigit(i)) {
