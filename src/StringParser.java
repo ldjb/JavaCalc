68e90079d3d40cac8e0f6cb61e7d56667965416f
Leon Byford <ldjb20@bath.ac.uk>
2012-12-23 15:47:30 +0000

    StringParser now allows multiple-digit natural numbers

--- src/StringParser.java
+++ src/StringParser.java
@@ -1,15 +1,31 @@
+// input: string inputted by user
+// output: array formatted for shunting-yard converter
+
 import java.util.*;
 public class StringParser {
 	public static Object[] parse(String input) {
 		ArrayList<Object> arrInput = new ArrayList<Object>();
+		boolean flgDigitEntered = false;
+		Double dblCurrentValue = new Double(0);
 		for (char i : input.toCharArray()) {
 			if (Character.isDigit(i)) {
-				arrInput.add(new Double(Character.getNumericValue(i)));
+				flgDigitEntered = true;
+				dblCurrentValue = dblCurrentValue * 10 +
+								  new Double(Character.getNumericValue(i));
 			}
 			else {
+				if (flgDigitEntered) {
+					arrInput.add(dblCurrentValue);
+					dblCurrentValue = new Double(0);
+					flgDigitEntered = false;
+				}
 				arrInput.add(Character.toString(i));
 			}
 		}
+		if (flgDigitEntered) {
+			arrInput.add(dblCurrentValue);
+		}
+		//System.out.println(arrInput);
 		return arrInput.toArray();
 	}
 }
