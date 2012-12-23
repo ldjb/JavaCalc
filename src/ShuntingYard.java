e2a2724c7134fca89b47e3c64847852bdb14fed7
Leon Byford <ldjb20@bath.ac.uk>
2012-12-23 22:15:02 +0000

    most of shunting yard done
    still need to handle operators (i.e. +,-,*,/)

--- src/ShuntingYard.java
+++ src/ShuntingYard.java
@@ -1,5 +1,29 @@
+import java.util.*;
 public class ShuntingYard {
 	public static Object[] convert(Object[] input) {
-		return
+		ArrayList<Object> outputQueue = new ArrayList<Object>();
+		Stack<Object> operatorStack = new Stack<Object>();
+		for (Object i : input) {
+			if (i.getClass().getName() == "java.lang.Double") {
+				outputQueue.add(i);
+			}
+			else if (i.equals("(")) {
+				operatorStack.push(i);
+			}
+			else if (i.equals(")")) {
+				while (!operatorStack.peek().equals("(")) {
+					outputQueue.add(operatorStack.pop());
+				}
+				operatorStack.pop();
+			}
+			else {
+				System.out.println("operator");
+			}
+		}
+		while (!operatorStack.empty()) {
+			outputQueue.add(operatorStack.pop());
+		}
+		System.out.println(outputQueue);//***
+		return outputQueue.toArray();
 	}
 }
