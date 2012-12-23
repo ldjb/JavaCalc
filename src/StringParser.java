a99d930cf0ffc731e9f398f5bcdf4df2ad71ffb7
Leon Byford <ldjb20@bath.ac.uk>
2012-12-23 17:10:47 +0000

    allows doubles in input

--- src/StringParser.java
+++ src/StringParser.java
@@ -1,4 +1,3 @@
-// allow doubles, negatives
 // input: string inputted by user
 // output: array formatted for shunting-yard converter
 
@@ -8,18 +7,32 @@ public class StringParser {
 		ArrayList<Object> arrInput = new ArrayList<Object>();
 		boolean flgDigitEntered = false;
 		boolean flgNegMode = false;
+		boolean flgFractionMode = false;
 		Double dblCurrentValue = new Double(0);
+		int decimalPlaceCounter = 0;
 		for (char i : input.toCharArray()) {
 			if (Character.isDigit(i)) {
 				flgDigitEntered = true;
 				dblCurrentValue = dblCurrentValue * 10 +
 								  new Double(Character.getNumericValue(i));
+				if (flgFractionMode) {
+					decimalPlaceCounter++;
+				}
 			}
 			else if (i == '-' && flgDigitEntered == false) {
 				flgNegMode = !flgNegMode;
 			}
+			else if (i == '.') {
+				flgFractionMode = true;
+			}
 			else if (i != ' ') {
 				if (flgDigitEntered) {
+					if (flgFractionMode) {
+						dblCurrentValue = dblCurrentValue / Math.pow(
+										  10, decimalPlaceCounter);
+						decimalPlaceCounter = 0;
+						flgFractionMode = false;
+					}
 					if (flgNegMode) {
 						arrInput.add(-dblCurrentValue);
 						flgNegMode = false;
@@ -34,6 +47,10 @@ public class StringParser {
 			}
 		}
 		if (flgDigitEntered) {
+			if (flgFractionMode) {
+				dblCurrentValue = dblCurrentValue /
+								  Math.pow(10, decimalPlaceCounter);
+			}
 			if (flgNegMode) {
 				arrInput.add(-dblCurrentValue);
 			}
