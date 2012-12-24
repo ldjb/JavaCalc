0f64e7550e3ee4adadf5d2daac1860a318b88173
Leon Byford <ldjb20@bath.ac.uk>
2012-12-24 15:03:01 +0000

    ShuntingYard.java should be working, but still seems broken

--- src/ShuntingYard.java
+++ src/ShuntingYard.java
@@ -3,6 +3,11 @@ public class ShuntingYard {
 	public static Object[] convert(Object[] input) {
 		ArrayList<Object> outputQueue = new ArrayList<Object>();
 		Stack<Object> operatorStack = new Stack<Object>();
+		Map<String, Object[]> operatorData = new HashMap<String, Object[]>();
+		operatorData.put("*", new Object[] {3, "left"});
+		operatorData.put("/", new Object[] {3, "left"});
+		operatorData.put("+", new Object[] {2, "left"});
+		operatorData.put("-", new Object[] {2, "left"});
 		for (Object i : input) {
 			if (i.getClass().getName() == "java.lang.Double") {
 				outputQueue.add(i);
@@ -17,7 +22,17 @@ public class ShuntingYard {
 				operatorStack.pop();
 			}
 			else {
-				System.out.println("operator");
+				while (!operatorStack.empty() &&
+					   ((operatorData.get(i)[1].equals("left") &&
+					   Integer.parseInt(operatorData.get(i)[0].toString()) <=
+					   Integer.parseInt(operatorData.get(
+					   operatorStack.peek())[0].toString())) ||
+					   (Integer.parseInt(operatorData.get(i)[0].toString()) <
+					   Integer.parseInt(operatorData.get(
+					   operatorStack.peek())[0].toString())))) {
+					outputQueue.add(operatorStack.pop());
+				}
+				operatorStack.push(i);
 			}
 		}
 		while (!operatorStack.empty()) {
