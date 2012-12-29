bde512e2143a8f6d0f72fad3a4455baa57cf8481
Leon Byford <ldjb20@bath.ac.uk>
2012-12-29 19:07:47 +0000

    GUI: cleanup + enhancements

--- src/CalcGUI.java
+++ src/CalcGUI.java
@@ -1,92 +1,83 @@
 import javax.swing.*;
 public class CalcGUI extends JFrame {
 
-    public CalcGUI() {
-        initComponents();
-    }
+	public CalcGUI() {
+		initComponents();
+	}
 
-    private void initComponents() {
+	private void initComponents() {
 
-        TextField = new JTextField();
-        Button = new JButton();
+		TextField = new JTextField();
+		Button = new JButton();
 
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Java Calculator");
-        setResizable(false);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setTitle("Java Calculator");
+		setResizable(false);
 
-        TextField.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
-        TextField.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                TextFieldActionPerformed(evt);
-            }
-        });
+		TextField.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
+		TextField.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				TextFieldActionPerformed(evt);
+			}
+		});
 
-        Button.setText("Calculate");
-        Button.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        Button.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                ButtonActionPerformed(evt);
-            }
-        });
+		Button.setText("Calculate");
+		Button.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+		Button.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				ButtonActionPerformed(evt);
+			}
+		});
 
-        GroupLayout layout = new GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
-                    .addComponent(Button)
-                    .addComponent(TextField, GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE))
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(TextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(Button)
-                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
+		GroupLayout layout = new GroupLayout(getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setHorizontalGroup(
+			layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+			.addGroup(layout.createSequentialGroup()
+				.addContainerGap()
+				.addGroup(layout.createParallelGroup(
+						  GroupLayout.Alignment.CENTER)
+					.addComponent(Button)
+					.addComponent(TextField, GroupLayout.DEFAULT_SIZE,
+								  388, Short.MAX_VALUE))
+				.addContainerGap())
+		);
+		layout.setVerticalGroup(
+			layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+			.addGroup(layout.createSequentialGroup()
+				.addContainerGap()
+				.addComponent(TextField, GroupLayout.PREFERRED_SIZE,
+							  GroupLayout.DEFAULT_SIZE,
+							  GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+				.addComponent(Button)
+				.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
 
-        pack();
-    }
+		pack();
+	}
 
-    private void TextFieldActionPerformed(java.awt.event.ActionEvent evt) {
-        // TODO add your handling code here:
-    }
+	private void TextFieldActionPerformed(java.awt.event.ActionEvent evt) {
+		TextField.setText("Hello, world!");
+		TextField.requestFocus();
+		TextField.selectAll();
+	}
 
-    private void ButtonActionPerformed(java.awt.event.ActionEvent evt) {
-        TextField.setText("Hello, world!");
-    }
+	private void ButtonActionPerformed(java.awt.event.ActionEvent evt) {
+		TextField.setText("Hello, world!");
+		TextField.requestFocus();
+		TextField.selectAll();
+	}
 
-    public static void main(String args[]) {
-        try {
-            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CalcGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CalcGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CalcGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CalcGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
+	public static void main(String args[]) {
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new CalcGUI().setVisible(true);
+			}
+		});
+	}
 
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new CalcGUI().setVisible(true);
-            }
-        });
-    }
-
-    private JButton Button;
-    private JTextField TextField;
+	private JButton Button;
+	private JTextField TextField;
 
 }
