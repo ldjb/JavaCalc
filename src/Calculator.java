41c5d1f404ce791f57607b156e7c270095196795
Leon Byford <ldjb20@bath.ac.uk>
2013-01-01 20:16:39 +0000

    document Extension.java (finished)

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,38 +1,44 @@
 /*******************************************************************************
-** Calculator.java [work in progress]                                         **
+** Calculator.java                                                            **
 ** -------------------------------------------------------------------------- **
-** Contains main() method and communicates with the other components of the   **
-** program.                                                                   **
+** Contains main() method. Initialises GUI mode if possible, otherwise        **
+** interacts with user in CLI mode.                                           **
 ** -------------------------------------------------------------------------- **
 ** Developed by Leon Byford <ldjb20@bath.ac.uk>                               **
-** Last modified: •••                                                         **
-** -------------------------------------------------------------------------- **
-** [To do]                                                                    **
-** • Extension.txt.                                                           **
-** • Documentation.                                                           **
+** Last modified: 1 January 2013                                              **
 *******************************************************************************/
 
+// import packages
 import java.awt.*;
 import java.io.*;
 import java.util.*;
 public class Calculator {
+	// command-line interface (CLI) mode
 	private static void cliMode() {
+		// BufferedReader for retrieving input from CLI
 		BufferedReader stdin = new BufferedReader(
 							   new InputStreamReader(System.in));
+		// iterate indefinitely
 		while (true) {
 			try {
+				// retrieve user input
 				String input = stdin.readLine();
 				if (input.toUpperCase().equals("QUIT")) {
+					// if user enters "QUIT" command, break out of loop and quit
 					break;
 				}
 				else if (input.equals("")) {
+					// if input is "", continue to next line of input
 					continue;
 				}
+				// determine output
 				String output = CalcIO.process(input);
+				// if output is null, an error has occurred so display message
 				if (output == null) {
 					System.err.println("Error. Please try again.");
 				}
 				else {
+					// display output
 					System.out.println(output);
 				}
 			}
@@ -48,10 +54,12 @@ public class Calculator {
 		}
 	}
 	public static void main(String[] args) {
+		// if "FORCE-CLI" argument has been used, force CLI mode
 		if (args.length > 0 && args[0].toUpperCase().equals("FORCE-CLI")) {
 			cliMode();
 		}
 		else {
+			// try to initialise graphical user interface (GUI) mode
 			try {
 				CalcGUI.initAll();
 			}
